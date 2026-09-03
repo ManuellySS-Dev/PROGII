b7e5648f4855f8f0d12e7bf5b41efb9b45b43e9e
ManuellyProg2 <manuelly.silva@edu.ufes.br>
2026-09-03 17:54:47 -0300

    só falta a main do condominio

---
+++ condominio/Respostas/Manuelly/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "area.h"
+#include "morador.h"
+#include "data.h"
+#include "reserva.h"
+#include <string.h>
+
+int main(){
+    int qtd_areas=0, i=0, qtd_moradores=0, qtd_reservas=0, indice_morador=0, indice_area=0, qtd_convidados=0, n_reservas=0, qtd_solicitacoes=0;
+    char cpf[MAX_TAM_CPF], identificador_area[10];
+    Data datareserva;
+    Reserva agendareservas[1000];
+
+    scanf("%d", &qtd_areas);
+    Area area[qtd_areas];
+
+    for(i=0; i<qtd_areas; i++){
+        area[i] = lerArea();
+    }
+
+    scanf("%d", &qtd_moradores);
+    Morador morador[qtd_moradores];
+
+    for(i=0; i<qtd_moradores; i++){
+        morador[i] = lerMorador();
+    }
+
+    scanf("%d", &qtd_solicitacoes);
+    for(i=0; i<qtd_solicitacoes; i++){
+        //verifica qual das areas pertence esse identificador
+        scanf(" %[^\n]", identificador_area);
+        scanf(" %[^\n]", cpf);
+        datareserva = lerData();
+        scanf("%d", &qtd_convidados);
+        for(indice_area=0; indice_area<qtd_areas; indice_area++){
+            if(verificaIdArea(area[indice_area], identificador_area)){
+                break;
+            }
+        }
+        if(indice_area == qtd_areas){
+            printf("nao tem essa area\n");
+            continue;
+        }
+        //verifica a qual dos moradores pertence esse cpf
+        for(indice_morador=0; indice_morador<qtd_moradores; indice_morador++){
+            if(verificaCPFMorador(morador[indice_morador], cpf)){
+                break;
+            }
+        }
+        if(indice_morador == qtd_moradores){
+            printf("nao tem esse morador\n");
+            continue;
+        }
+
+        if(verificaSolicitacaoReserva(agendareservas, n_reservas, morador[indice_morador], area[indice_area], datareserva, qtd_convidados) == 1){
+            imprimeReserva(agendareservas[n_reservas]);
+            n_reservas++;
+        }
+    }
+
+
+return 0;
+}
